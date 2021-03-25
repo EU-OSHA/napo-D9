8171a3240be22dd5b38ec102c5d0cdb9cc32a19d
Josu Jorquera <jmjorquera@bilbomatica.es>
2021-03-25 13:03:38 +0100

    MNAPO-37 - Paginator Styles

--- themes/custom/napo_theme/js/global.js
+++ themes/custom/napo_theme/js/global.js
@@ -12,5 +12,21 @@
 
     }
   };
+  // Pager index
+  $('.pagination').each(function () {
+    let itemLength = $(this).find('.page-item').length;
+    let lastItemIndex=itemLength - 1;
+    $(this).find('.page-item').eq(lastItemIndex).addClass('last');
+
+    let itemFirst = $(this).find('[title="Go to first page"]').length;
+    if(itemFirst>0){
+      $(this).find('[title="Go to first page"]').closest('.page-item').addClass('first');
+    }
+
+    let itemPrev = $(this).find('[title="Go to previous page"]').length;
+    if(itemPrev>0){
+      $(this).find('[title="Go to previous page"]').closest('.page-item').addClass('prev');
+    }
+  });
 
 })(jQuery, Drupal);
