dd3fca38d03657b512466c17a5e17959714cd380
Josu Jorquera <jmjorquera@bilbomatica.es>
2021-04-29 11:08:47 +0200

    MNAPO-37 - New Theme - Styles For Responsive And Print

--- themes/custom/napo_theme/js/video.js
+++ themes/custom/napo_theme/js/video.js
@@ -1,5 +1,4 @@
 // Home video controls
 $( document ).ready(function() {
-  // $('.block-views-blockfrontpage-custom-video-block').after('<div class="play-button-container"><button type="buttom" class="button-play"></button></div><div class="video-controls"></div>');
 
 });
